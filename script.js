38f8e2aeacfd4e5d35d9971ca657b29c9054a87b
Alexsander Batista dos Santos <alexsanderbatistadossantos@yahoo.com.br>
2023-04-17 22:25:53 -0300

    create dropdown button

---
+++ script.js
@@ -0,0 +1,28 @@
+const btnEl = document.querySelector(".button-nav");
+const dropEls = document.querySelectorAll(".dropdown");
+
+btnEl.addEventListener("click", () => {
+  const imgEl = btnEl.querySelector(".button-img");
+  if (!document.body.classList.contains("mobile-open")) {
+    imgEl.setAttribute("src", "/images/icon-close-menu.svg");
+    return document.body.classList.add("mobile-open");
+  }
+
+  imgEl.setAttribute("src", "/images/icon-menu.svg");
+  return document.body.classList.remove("mobile-open");
+});
+
+dropEls.forEach((dropEl) => {
+  const navLinkEl = dropEl.querySelector(".nav-link");
+
+  navLinkEl.addEventListener("click", function () {
+    const arrowEl = dropEl.querySelector(".arrow-up");
+    if (!dropEl.classList.contains("drop-open")) {
+      arrowEl.setAttribute("src", "/images/icon-arrow-up.svg");
+      return dropEl.classList.add("drop-open");
+    }
+
+    arrowEl.setAttribute("src", "/images/icon-arrow-down.svg");
+    return dropEl.classList.remove("drop-open");
+  });
+});
