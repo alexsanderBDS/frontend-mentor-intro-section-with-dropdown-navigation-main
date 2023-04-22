607d1df295b3e283de74bf7526c1ee23e7bc7cb6
Alexsander Batista dos Santos <alexsanderbatistadossantos@yahoo.com.br>
2023-04-22 19:02:36 -0300

    replace script img src

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const dropEls = document.querySelectorAll(".dropdown");
 btnEl.addEventListener("click", () => {
   const imgEl = btnEl.querySelector(".button-img");
   if (!document.body.classList.contains("mobile-open")) {
-    imgEl.setAttribute("src", "/images/icon-close-menu.svg");
+    imgEl.setAttribute("src", "images/icon-close-menu.svg");
     return document.body.classList.add("mobile-open");
   }
 
-  imgEl.setAttribute("src", "/images/icon-menu.svg");
+  imgEl.setAttribute("src", "images/icon-menu.svg");
   return document.body.classList.remove("mobile-open");
 });
 
@@ -16,11 +16,11 @@ dropEls.forEach((dropEl) => {
   dropEl.addEventListener("click", function () {
     const arrowEl = dropEl.querySelector(".arrow-up");
     if (!dropEl.classList.contains("drop-open")) {
-      arrowEl.setAttribute("src", "/images/icon-arrow-up.svg");
+      arrowEl.setAttribute("src", "images/icon-arrow-up.svg");
       return dropEl.classList.add("drop-open");
     }
 
-    arrowEl.setAttribute("src", "/images/icon-arrow-down.svg");
+    arrowEl.setAttribute("src", "images/icon-arrow-down.svg");
     return dropEl.classList.remove("drop-open");
   });
 });
